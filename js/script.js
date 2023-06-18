53cb68135c73621fae18c33bf3eb43e224cab64c
agnaibat <136494270+agnaibat@users.noreply.github.com>
2023-06-18 20:42:11 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+AOS.init({offset: 90, once: false, duration: 1200,
+initClassName: 'aos-init',
+animatedClassName: 'aos-animate',
+useClassNames: false,
+disableMutationObserver: false,
+debounceDelay: 50,
+throttleDelay: 99,
+}); 
